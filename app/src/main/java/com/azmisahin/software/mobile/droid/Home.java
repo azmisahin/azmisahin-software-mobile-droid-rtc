69f62f6a1666ac938c53da1fa9b3e21efdecda40
Azmi ŞAHİN <azmisahin@outlook.com>
2020-01-21 20:53:13 +0300

    Added home controller and layout

---
+++ app/src/main/java/com/azmisahin/software/mobile/droid/Home.java
@@ -0,0 +1,14 @@
+package com.azmisahin.software.mobile.droid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.controller_home);
+    }
+}
