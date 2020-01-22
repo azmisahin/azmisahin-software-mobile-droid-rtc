10fc6ca8950e17f20d4f6d80cda166db72fd380d
Azmi ŞAHİN <azmisahin@outlook.com>
2020-01-22 11:30:52 +0300

    UI Home Controller

---
+++ app/src/main/java/com/azmisahin/software/mobile/droid/ui/Home/ControllerHome.java
@@ -0,0 +1,19 @@
+package com.azmisahin.software.mobile.droid.ui.Home;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.azmisahin.software.mobile.droid.R;
+
+/**
+ * Home Controller
+ */
+public class ControllerHome extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.controller_home);
+    }
+}
