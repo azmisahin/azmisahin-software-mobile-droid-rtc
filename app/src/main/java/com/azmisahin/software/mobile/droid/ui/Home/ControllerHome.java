791ea183ce2e651435c6c1b54413e6a35df033cc
Azmi ŞAHİN <azmisahin@outlook.com>
2020-01-22 12:18:46 +0300

    UI Login Controller

--- app/src/main/java/com/azmisahin/software/mobile/droid/ui/Home/ControllerHome.java
+++ app/src/main/java/com/azmisahin/software/mobile/droid/ui/Home/ControllerHome.java
@@ -1,11 +1,16 @@
 package com.azmisahin.software.mobile.droid.ui.Home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.azmisahin.software.mobile.droid.R;
+import com.azmisahin.software.mobile.droid.ui.Login.ControllerLogin;
+
+import java.util.ResourceBundle;
 
 /**
  * Home Controller
@@ -15,5 +20,14 @@ public class ControllerHome extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.controller_home);
+    }
+
+    /**
+     * Login Start
+     * @param view
+     */
+    public void login(View view) {
+        Intent intent = new Intent(this, ControllerLogin.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/azmisahin/software/mobile/droid/ui/Login/ControllerLogin.java
@@ -0,0 +1,18 @@
+package com.azmisahin.software.mobile.droid.ui.Login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.azmisahin.software.mobile.droid.R;
+
+/**
+ * Login Controller
+ */
+public class ControllerLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.controller_login);
+    }
+}
